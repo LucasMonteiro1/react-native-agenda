1316f6a171b9040407d119c6dbfcb022e9ba5912
Lucas Monteiro <lucasmanganellimonteiro@gmail.com>
2020-01-31 11:35:32 -0300

    update dependencies

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
+import { Agenda, LocaleConfig } from 'react-native-calendars';
 
 LocaleConfig.locales['pt-br'] = {
   monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
@@ -11,57 +11,51 @@ LocaleConfig.locales['pt-br'] = {
 
 LocaleConfig.defaultLocale = 'pt-br';
 
-export default class App extends Component {
-  state = {
-    calendarOpened: false,
-  }
+const App = () => {
+  const [calendarOpened, setCalendarOpened] = useState(false);
 
-  render() {
-    const { calendarOpened } = this.state;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={{ flex: 1 }}>
+        <Agenda
+          items={{
+            '2019-04-15': [{text: 'any js object'}],
+            '2019-04-16': [{text: 'item 1 - any js object'}],
+            '2019-04-17': [{text: 'item 2 - any js object'}, {text: 'item 3 - any js object'}],
+          }}
+          loadItemsForMonth={(month) => {console.log('trigger items loading')}}
+          onDayPress={(day)=>{console.log('day pressed')}}
+          onDayChange={(day)=>{console.log('day changed')}}
+          selected={'2019-04-17'}
+          minDate={'2000-01-01'}
+          maxDate={'2030-12-31'}
+          renderItem={(item, firstItemInDay) => (
+            <View style={[styles.item, { height: item.height }]}>
+              <Text>{item.text}</Text>
+            </View>
+          )}
+          renderEmptyDate={() => (
+            <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+          )}
+          onCalendarToggled={(calendarOpened) => setCalendarOpened(calendarOpened)}
+          renderEmptyData = {() => {return (<View><Text>empty data</Text></View>);}}
+          rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+          onRefresh={() => console.log('refreshing...')}
+          refreshing={false}
+          refreshControl={null}
+          calendarScrollable
+          theme={{
+            knobContainer: {
+              backgroundColor: 'red'
+            }
+          }}
+          style={{
 
-    return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-        <View style={{ flex: 1 }}>
-          <Agenda
-            items={{
-              '2019-04-15': [{text: 'any js object'}],
-              '2019-04-16': [{text: 'item 1 - any js object'}],
-              '2019-04-17': [{text: 'item 2 - any js object'}, {text: 'item 3 - any js object'}],
-            }}
-            loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-            onDayPress={(day)=>{console.log('day pressed')}}
-            onDayChange={(day)=>{console.log('day changed')}}
-            selected={'2019-04-17'}
-            minDate={'2000-01-01'}
-            maxDate={'2030-12-31'}
-            renderItem={(item, firstItemInDay) => (
-              <View style={[styles.item, { height: item.height }]}>
-                <Text>{item.text}</Text>
-              </View>
-            )}
-            renderEmptyDate={() => (
-              <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
-            )}
-            onCalendarToggled={(calendarOpened) => this.setState({ calendarOpened })}
-            renderEmptyData = {() => {return (<View><Text>empty data</Text></View>);}}
-            rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
-            onRefresh={() => console.log('refreshing...')}
-            refreshing={false}
-            refreshControl={null}
-            calendarScrollable
-            theme={{
-              knobContainer: {
-                backgroundColor: 'red'
-              }
-            }}
-            style={{
-
-            }}
-          />
-        </View>
-      </SafeAreaView>
-    );
-  }
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -85,3 +79,5 @@ const styles = StyleSheet.create({
     paddingTop: 30,
   },
 });
+
+export default App;
